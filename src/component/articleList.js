40cc3f8f6f0ac751f3acec17dce6fc2e5bf8c1ba
joker <740436391@qq.com>
2019-01-07 17:28:05 +0800

    reverse article list

--- src/component/articleList.js
+++ src/component/articleList.js
@@ -19,10 +19,10 @@ class ArticleList extends Component {
             <Container style={{padding: "0.5em 0", marginTop: "3em"}}>
                 <List divided selection relaxed className="article-list">
                     {
-                        Object.entries(articles).map( ([id, article]) => (
-                            <List.Item key={id} articleid={id} onClick={this.props.toArticle}>
+                        articles.map( (article, index) => (
+                            <List.Item key={index} articleid={index} onClick={this.props.toArticle}>
                                 <List.Content className="item" style={itemContentStyle}>
-                                    <Link to={'/article/' + id}>{ article.title }</Link>
+                                    <Link to={'/article/' + index}>{ article.title }</Link>
                                 </List.Content>
                             </List.Item>
                         ))
--- src/service/db.js
+++ src/service/db.js
@@ -2,9 +2,8 @@ import { article as JPB } from '../article/java_primitive_boxing'
 import { article as SBP } from '../article/spring_bean_proxy' 
 
 
-const articles = {
-    1: JPB,
-    2: SBP,
-}
+const articles = [
+    JPB, SBP
+].reverse();
 
 export default articles 
